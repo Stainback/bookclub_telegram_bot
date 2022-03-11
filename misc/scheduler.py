7462471703cfec73c978ed350cbf8830bf88def6
ANMI <migunov1au@gmail.com>
2022-03-11 20:55:29 +0200

    Scheduler has been rewritten due to MongoDB implementation

--- misc/scheduler.py
+++ misc/scheduler.py
@@ -4,6 +4,8 @@ from datetime import date
 import aioschedule
 
 from config import CHAT_ID, DATE_CLUB_BIRTHDAY
+
+from data_loader import collection_profiles, get_message_prompt, collection_meetings
 from loader import bot
 
 
@@ -12,19 +14,19 @@ async def send_daily_notification():
     today = date.isoformat(date.today())[5::]
 
     # Check if today is someone's birthday or/and club birthday
-    for profile in PROFILE_DATA:
-        if today == profile.data["birth_date"]:
+    birthday_profiles = collection_profiles.find({"birth_date": today})
+    if birthday_profiles is not None:
+        for profile in birthday_profiles:
+            await bot.send_message(CHAT_ID,
+                                   get_message_prompt("msg_birthday_001").replace("@username", profile["username"]))
 
-            await bot.send_message(CHAT_ID, MESSAGE_DATA["msg_birthday_001"].replace("@username", profile["username"]))
-
-        if today == DATE_CLUB_BIRTHDAY:
-
-            await bot.send_message(CHAT_ID, MESSAGE_DATA["msg_clubbirthday_001"])
+    if today == DATE_CLUB_BIRTHDAY:
+        await bot.send_message(CHAT_ID, get_message_prompt("msg_clubbirthday_001"))
 
     # Check if today is scheduled meeting
-    for meeting in MEETING_DATA:
-        if today == meeting.data["meeting_date"]:
-
+    todays_meetings = collection_meetings.find({"data": {"meeting_date": today}})
+    if todays_meetings is not None:
+        for meeting in todays_meetings:
             await bot.send_message(CHAT_ID, meeting.generate_meeting_message())
 
 
@@ -32,10 +34,10 @@ async def remove_obsolete_meetings():
     """This function is expected to be launched via Scheduler at 22:00 on daily basis."""
     today = date.isoformat(date.today())
 
-    for meeting in MEETING_DATA:
-        if meeting.data["meeting_date"] == today[5::]:
-            MEETING_DATA.pop(MEETING_DATA.index(meeting))
-            update_bot_data([meeting.data for meeting in MEETING_DATA], "data/data_meetings.json")
+    todays_meetings = collection_meetings.find({"data": {"meeting_date": today}})
+    if todays_meetings is not None:
+        for meeting in todays_meetings:
+            collection_meetings.delete_one({"_id": meeting["_id"]})
 
 
 async def scheduler():
